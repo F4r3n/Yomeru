b2fda621c16cf2cc878a4887d2d1d33dd97952d7
f4r3n <guillaume_k@hotmail.fr>
2026-05-23 13:39:42 +0100

    fix(extension): use object init syntax and add #main mount target
    wasm-pack's init() now expects { module_or_path } instead of a bare URL.
    Add <div id="main"> so Dioxus mounts there rather than falling back to body.

--- extension/options-dx-loader.js
+++ extension/options-dx-loader.js
@@ -2,5 +2,5 @@ import init from "./_generated/yomeru-extension/yomeru_extension.js";
 const wasmUrl = browser.runtime.getURL(
   "_generated/yomeru-extension/yomeru_extension_bg.wasm",
 );
-await init(wasmUrl);
+await init({ module_or_path: wasmUrl });
 document.getElementById("loading")?.remove();
